63499a135df9ea89dfc5662f77c75a470e12e602
farukates99 <80276059+farukates99@users.noreply.github.com>
2021-03-12 00:26:12 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,7 @@
-#Explain your work
+oddnumbers = [1,3,5,7,9]
+evennumbers = [0,2,4,6,8]
+numbers = oddnumbers + evennumbers  #merge two list
 
-#Question 1
-for x in range(a):
-	print(a)
+numbers= [i*2 for i in numbers ]    # multiply all values of the new list by 2
+for item in numbers:
+    print (type(item))          # data type of the all values in the new list
